c34b78e410bd92b3249c31532d65474cad0fe398
Che <che.glenn@csedge.org>
2018-10-06 15:45:33 -0400

    Challenge 1
    Completed

--- data.js
+++ data.js
@@ -1,5 +1,20 @@
 // Example
 let message = "Hello World!";
-console.log(message);
+//number variables
+let i = 1;
+let love = 2;
+let you = 3;
+//boolean variables
+let imcool = true;
+let hate you = false;
+//string variables
+let color = "red";
+let name = 'che';
+let lastname = 'glenn';
+//undefined variables
+//random
+constant READLINE = require("readline-sync")
+let userInput = READLINE.question("Type anything and I'll repeat it back!");
+console.log('You typed: ${userInput}');
 
-// Type your code below!
+
