639731b548025d5d37d29d505f6a7037e4484399
Che <che.glenn@csedge.org>
2018-10-13 12:00:26 -0400

    Finished math.js
    Yeet

--- math.js
+++ math.js
@@ -11,33 +11,42 @@ console.log(sum);
 // **** Problem 2 ****
 // Store the difference between sum (above) and 7 in a variable.
 // Print the difference.
-
+let num3 = 7;
+let difference = sum - num3;
+console.log(difference);
 
 // **** Problem 3 ****
 // Store the product of difference and 3 in a variable.
 // Print the product.
-
+let num4 = 3;
+let product = difference * num4;
+console.log(product);
 
 // **** Problem 4 ****
 // Store the power of the product raised to an exponent of 2 in a variable.
 // Print the power.
-
+let num5 = 2;
+let power = product ** num5;
+console.log(power);
 
 // **** Problem 5 ****
 // Store the quotient of the power divided by 4 in a variable.
 // Print the quotient.
-
+let num6 = 4;
+let quotient = power / num6;
+console.log(quotient);
 
 // **** Problem 6 ****
 // Store the remainder of the quotient divided by 2 in a variable.
 // Print the remainder.
-
+let num7 = 2;
+let remainder = quotient % num7;
+console.log(remainder);
 
 // **** Problem 7 ****
 // Print a boolean value (true/false) representing if the quotient from
 // problem 5 is even or odd. If the quotient is even, print true. If the
 // quotient is odd, print false.
-//
 // Hint: You can tell if a value is even or odd if it is divisible by 2;
 // in other words, if there is no remainder after dividing the value by
 // 2, then the value is even. See problem 6!
