a5518600dd8a57039f4328712bbc90d12b038f3c
Che <che.glenn@csedge.org>
2018-10-20 12:15:48 -0400

    Succy Duccy
    done ez

---
+++ object.js
@@ -0,0 +1,19 @@
+let hacker = {
+	firstName: "Putin",
+	nickName: "Rasputin",
+	catchPhrase: "за Родину",
+	archEnemy: "Murica"
+}; 
+let villain = {
+	nickName: "Murica",
+	catchPhrase: "yeet",
+	greatestCrime: "dabbing on all the haters"
+};
+console.log(`Hi my name is ${hacker.firstName}
+	But you can call me ${hacker.nickName}
+	My secret catch phrase is ${hacker.catchPhrase}
+
+	My arch enemy is ${villain.nickName}
+	Their horrid catch phrase is ${villain.catchPhrase}
+	The greatest crime they have commited is ${villain.greatestCrime}
+	`);
